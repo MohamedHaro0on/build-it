eb7867b789b9c9c29fcd324078418ea0ea3558f5
MohamedHaro0on <67293364+MohamedHaro0on@users.noreply.github.com>
2021-04-16 07:00:34 +0200

    Updates

---
+++ precache-manifest.fe16cb7a9ea4d30f12e60f19ce052e46.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d59037e135029718dfb30c770d70a795",
+    "url": "/build-it/index.html"
+  },
+  {
+    "revision": "c868fd1003fc20337c35",
+    "url": "/build-it/static/css/main.66370ec2.chunk.css"
+  },
+  {
+    "revision": "c877e65a001addc9703d",
+    "url": "/build-it/static/js/2.43c7d7d5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/build-it/static/js/2.43c7d7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c868fd1003fc20337c35",
+    "url": "/build-it/static/js/main.6cfe6952.chunk.js"
+  },
+  {
+    "revision": "02f588a05527e1bf738c",
+    "url": "/build-it/static/js/runtime-main.30ba6337.js"
+  },
+  {
+    "revision": "4648bebc3f43421728fff8f4e1b1ee82",
+    "url": "/build-it/static/media/bull.4648bebc.svg"
+  },
+  {
+    "revision": "4fb170c50f0e19f10a9b4e3ae850082b",
+    "url": "/build-it/static/media/cube.4fb170c5.svg"
+  },
+  {
+    "revision": "85387c168d8c85774453610cc7e9edd4",
+    "url": "/build-it/static/media/r2d2.85387c16.svg"
+  },
+  {
+    "revision": "dc65526de4f50130c07be627db3e4e3e",
+    "url": "/build-it/static/media/spaceman.dc65526d.svg"
+  },
+  {
+    "revision": "9ab0b316d72490c90c3e1fd40cff91c0",
+    "url": "/build-it/static/media/staticmap (1).9ab0b316.jpg"
+  },
+  {
+    "revision": "534c81a3cd149f2c9b3936653a647417",
+    "url": "/build-it/static/media/staticmap (2).534c81a3.jpg"
+  },
+  {
+    "revision": "0ebd60484d4d1b43986771800f78c55a",
+    "url": "/build-it/static/media/staticmap (3).0ebd6048.jpg"
+  },
+  {
+    "revision": "673a4dd1e84d31234da4f8f6446c441d",
+    "url": "/build-it/static/media/staticmap (4).673a4dd1.jpg"
+  },
+  {
+    "revision": "912e6a2ba9b0323696ece7ef892e727e",
+    "url": "/build-it/static/media/staticmap (5).912e6a2b.jpg"
+  },
+  {
+    "revision": "be8531f8a5b9f57b5cf6d8595dd7ea44",
+    "url": "/build-it/static/media/staticmap (6).be8531f8.jpg"
+  },
+  {
+    "revision": "eca5726d0ea231dea09e6191b643bf7f",
+    "url": "/build-it/static/media/staticmap.eca5726d.jpg"
+  }
+]);
